f2a638fa1b1f383bb3253045043a6dee8457e918
arash-akhlaghi <arash.akhlaghi.d@gmail.com>
2025-07-01 05:30:38 -0700

    asil

---
+++ asil.py
@@ -0,0 +1,18 @@
+print("Asil")
+# i have write this after first commit 
+print("Hello")
+# i have write this after second try 
+print("Second")
+#now we are in branch. what should i do 
+print("it is four?")
+# now we are in branch 
+print("we are in branch")
+# it is after barnching is down 
+print("Done")
+#hello
+print("after cloning")
+# it is someting after ...
+print("netrunner")
+# i delete that 
+print("delete")
+# it is a test for demonsrate pink color 
